696394dc6596699ffe1440862afdd340ac8ff17c
Pobx <geidtiphong.s@allrightbox.com>
2020-07-29 10:59:31 +0700

    add migrations file

---
+++ Migrations/20200729035214_AddLabelColumnAndDefaultValue.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_ef_core.Migrations
+{
+    public partial class AddLabelColumnAndDefaultValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Label",
+                table: "Orders",
+                nullable: false,
+                defaultValue: "Pobx");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Label",
+                table: "Orders");
+        }
+    }
+}
